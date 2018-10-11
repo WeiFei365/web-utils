0be5dd2d9a87a373c053431b6f9acbd28bbc1526
weifei365 <weifei365@gmail.com>
2018-10-11 13:34:05 +0800

    修复 正则模板 中的错误，主要是： 1、未去除字符中正则内置特殊字符； 2、增加字符数量的限制，因为在正则中 $n 特殊变量的最大值是 $99；（当然，因为本次将 replacement 换成了 function，已经不会受该限制，但是还是为了安全）； 3、修复 global 未生效的问题，将 (.*) 更改为 (.*?)； 5、优化 replacement 替换策略，支持子串内重复字符的替换，如：'abac' 会被替换为 '[a]b[a][c]'

--- src/regex/search-char.js
+++ src/regex/search-char.js
@@ -1,34 +1,51 @@
 import _isArray from 'lodash/isArray';
+import {
+  regexSPS
+} from './const';
 
 
 /**
- * [编译用于按字符来匹配/高亮字符串的正则模板]
+ * [编译用于按字符来匹配/高亮字符串的正则模板, 会过滤掉正则中的特殊字符]
  * @author WEIFEI
  * @method regexSearchChar
  * @param  {Array}         [chars=[]] [待编译的字符数组]
  * @param  {String}        [opts='']  [实例化 RegExp 的第二个参数, 不建议用]
  * @return {Object}                   [编译包结构如下:]
  *                                    {
- *                                        test: 用于测试某字符串是否包含chars中的字符
- *                                        replace: string.replace 函数的第一个参数
- *                                        replacement: string.replace 函数的第二个参数
+ *                                        @param RegExp test: 用于测试某字符串是否包含chars中的字符
+ *                                        @param RegExp replace: string.replace 函数的第一个参数
+ *                                        @param Function replacement: string.replace 函数的第二个参数
  *                                    }
  */
 export default function regexSearchChar(chars = [], opts = '') {
   if (!_isArray(chars)) {
     return {};
   }
-  chars = chars.map((d) => String(d)).filter((d) => !!d);
+  chars = chars.map((d) => String(d))
+    .filter((d) => !!d && regexSPS().indexOf(d) === -1)
+    // 正则表达式最多支持到 $99
+    .slice(0, 98);
   if (!chars.length) {
     return {};
   }
-  const replacement = [];
-  chars.forEach((char, index) => replacement.push(`$${index + 1}<em>${char}</em>`));
-  replacement.push(`$${chars.length + 1}`);
+
+  const pattern = `(.*?)${chars.join('(.*?)')}(.*?)`;
 
   return {
-    test: new RegExp(`(.*)${chars.join('(.*)')}(.*)`, opts || 'i'),
-    replace: new RegExp(`(.*)${chars.join('(.*)')}(.*)`, opts || 'ig'),
-    replacement: replacement.join(''),
+    // 用于检测字符串是否符合需要
+    test: new RegExp(pattern, opts || 'i'),
+    // 用于匹配出字符串中所有符合需要的子字符串
+    replace: new RegExp(pattern, opts || 'ig'),
+    // 用于替换所有匹配到的子串
+    replacement: (...args) => {
+      const items = args.slice(1, chars.length + 2);
+      const strs = [items[0]];
+      for (let i = 1; i < items.length; i++) {
+        const c = chars[i - 1];
+        strs.push(`<em>${c}</em>`);
+        strs.push(items[i].replace(new RegExp(c, 'ig'), `<em>${c}</em>`));
+      }
+      return strs.join('');
+    },
   };
 }
--- test/regex/search-char.test.js
+++ test/regex/search-char.test.js
@@ -7,17 +7,33 @@ describe('regex/search-char', function() {
     [
       [undefined, undefined, {}],
       ['ab', undefined, {}],
-      [['a', 'b'], undefined, {
-        test: /(.*)a(.*)b(.*)/i,
-        replace: /(.*)a(.*)b(.*)/ig,
-        replacement: '$1<em>a</em>$2<em>b</em>$3',
-      }],
       [['', ''], undefined, {}],
     ].forEach((d) => {
       const chars = d[0];
       const opts = d[1];
       it(`regexSearchChar(${chars}, ${opts}) should return ${jsonFrom.default(d[2])}`, function() {
         chai.assert.deepStrictEqual(regexSearchChar.default(chars, opts), d[2]);
+      });
+    });
+    [
+      [['a', 'b'], undefined, 'a', [false, 'a']],
+      [['a'], undefined, 'a', [true, '<em>a</em>']],
+      [['a'], undefined, 'abac', [true, '<em>a</em>b<em>a</em>c']],
+      [['a', 'b'], undefined, 'ab', [true, '<em>a</em><em>b</em>']],
+      [['a', 'b'], undefined, '_ab', [true, '_<em>a</em><em>b</em>']],
+      [['a', 'b'], undefined, 'ab_', [true, '<em>a</em><em>b</em>_']],
+      [['a', 'b'], undefined, 'a_b', [true, '<em>a</em>_<em>b</em>']],
+      [['a', '|', 'b'], undefined, 'ab', [true, '<em>a</em><em>b</em>']],
+      [['a', 'b'], undefined, 'acab', [true, '<em>a</em>c<em>a</em><em>b</em>']],
+    ].forEach((d) => {
+      const chars = d[0];
+      const opts = d[1];
+      it(`regexSearchChar(${chars}, ${opts}) and ${d[2]} should return ${d[3]}`, function() {
+        const regex = regexSearchChar.default(chars, opts);
+        chai.assert.deepStrictEqual([
+          regex.test.test(d[2]),
+          d[2].replace(regex.replace, regex.replacement)
+        ], d[3]);
       });
     });
   });
