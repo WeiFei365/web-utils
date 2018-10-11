8fc76ec8ef0cc2a95d15930c38dda80a6fa42c7d
weifei365 <weifei365@gmail.com>
2018-10-11 11:54:46 +0800

    1、新增正则：用以过滤正则中内置的特殊字符； 2、更改静态变量的使用，避免被我不修改；

--- test/regex/const.test.js
+++ test/regex/const.test.js
@@ -1,11 +1,12 @@
 const {
   regexPhone,
   regexEmail,
+  regexSPS,
 } = require('../../src/regex/const.js');
 
 
 describe('regex/const', function() {
-  describe('@regexPhone', function() {
+  describe('@regexPhone()', function() {
     [
       ['15506000365', true],
       ['1550600036', false],
@@ -14,12 +15,12 @@ describe('regex/const', function() {
       ['12345678901', true],
       ['123456789012', false],
     ].forEach((d) => {
-      it(`regexPhone:test:${d[0]} should return ${d[1]}`, function() {
-        chai.assert.strictEqual(regexPhone.test(d[0]), d[1]);
+      it(`regexPhone():test:${d[0]} should return ${d[1]}`, function() {
+        chai.assert.strictEqual(regexPhone().test(d[0]), d[1]);
       });
     });
   });
-  describe('@regexEmail', function() {
+  describe('@regexEmail()', function() {
     [
       ['abc@gmail.com', true],
       ['abc@gmail.', false],
@@ -30,8 +31,19 @@ describe('regex/const', function() {
       ['-@gmail.com', true],
       ['_@gmail.com', true],
     ].forEach((d) => {
-      it(`regexEmail:test:${d[0]} should return ${d[1]}`, function() {
-        chai.assert.strictEqual(regexEmail.test(d[0]), d[1]);
+      it(`regexEmail():test:${d[0]} should return ${d[1]}`, function() {
+        chai.assert.strictEqual(regexEmail().test(d[0]), d[1]);
+      });
+    });
+  });
+  describe('@regexSPS()', function() {
+    [
+      ['a', true],
+      ['|', false],
+      ['\\', false],
+    ].forEach((d) => {
+      it(`regexSPS().indexOf(${d[0]}) should return ${d[1]}`, function() {
+        chai.assert.strictEqual(regexSPS().indexOf(d[0]) === -1, d[1]);
       });
     });
   });
