4e8acd01048ca41699afddd5a40e938ec4246706
weifei365 <weifei365@gmail.com>
2018-10-19 23:34:13 +0800

    更新 fetch 的基础逻辑：当发生 http 状态码或网络级的错误时， reject 给上层处理，其他情况会 resolve 给上层

--- test/fetch/fetch-post.test.js
+++ test/fetch/fetch-post.test.js
@@ -5,8 +5,8 @@ describe('fetch/fetch-post', function() {
   describe('@fetchPost(pathname, data, options, host)', function() {
     it(`fetchPost(undefined, undefined, undefined, undefined) should return code=20000`, function(done) {
       fetchPost().then((data) => {
-        done(data.code === 20000 ? undefined : (data.error || new Error('-test')))
-      });
+        done(new Error('-test'));
+      }, () => done());
     });
     it(`fetchPost(/_private/browser/stats, undefined, undefined, https://api.github.com) should return {}`, function(done) {
       fetchPost('/_private/browser/stats', undefined, undefined, 'https://api.github.com').then((data) => {
@@ -20,8 +20,8 @@ describe('fetch/fetch-post', function() {
     });
     it(`fetchPost(/_private/browser/stats, { q: 'web-utils' }, { headers: { user: test } }, https://api.github.com) should return code=20000`, function(done) {
       fetchPost('/_private/browser/stats', { q: 'web-utils' }, { headers: { user: 'test' } }, 'https://api.github.com').then((data) => {
-        done((data && data.code === 20000) ? undefined : (data.error || new Error('-test')));
-      });
+        done(new Error('-test'));
+      }, () => done());
     });
   });
 });
