c039e5e3cbeeae436ed2ec822812edc8b30fddca
weifei365 <weifei365@gmail.com>
2019-01-30 10:29:42 +0800

    支持更多的作用

--- src/other/vali-filename.js
+++ src/other/vali-filename.js
@@ -2,15 +2,16 @@
  * [校验文件名，去除非法的字符]
  * @author WEIFEI
  * @method valiFilename
- * @param  {String}     name          [文件/文件夹名]
- * @param  {String}     [dftChar='-'] [非法字符的替换]
- * @return {String}                   [description]
+ * @param  {String}     name            [文件/文件夹名]
+ * @param  {String}     [dftChar='-']   [非法字符的替换]
+ * @param  {Number}     [maxLength=250] [返回字串的最大长度]
+ * @return {String}                     [description]
  */
-export default function valiFilename(name, dftChar = '-') {
+export default function valiFilename(name, dftChar = '-', maxLength = 250) {
   return String(typeof name === 'string' ? name : '')
   .replace(/\/|\\|>|<|\?|:|"|'|\^|!|;|\*|&|%|\$|#|@/g, '?')
   .replace(/\?+/g, dftChar)
   .replace(/\n|\f|\r|\t|\v/g, ' ')
   .replace(/\s+/g, ' ')
-  .substr(0, 250);
+  .substr(0, maxLength);
 }
